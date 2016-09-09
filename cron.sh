11180e9d48c83779a0378ee1e9e0d0e40cd8311a
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-09-10 02:20:30 +0530

    New posts

--- cron.sh
+++ cron.sh
@@ -1,9 +1,10 @@
-cd /home/sushant/code/www.supplychainplanet.site
-nikola build
-nikola build
-nikola build
+cd /home/sushant/code/supply_chain_planet_site/www.supplychainplanet.site
+for i in $(seq 3)
+do
+	nikola build
+done;
 
-s3cmd output/ s3://www.supplychainplanet.site/
+s3cmd sync output/ s3://www.supplychainplanet.site/
 git add .
 git commit -m"New posts"
 git push
